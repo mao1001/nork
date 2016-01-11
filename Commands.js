f981448866de0e95478a5517509d3130cfedead7
C Nicholas Mao <mao@tacktechnologies.com>
2016-01-11 15:35:50 -0800

    Converted ItemDatabase to a JSON file. Created world.json

--- Commands.js
+++ Commands.js
@@ -2,9 +2,18 @@
 
 var validCommands = ["GO", "TAKE", "USE", "INVENTORY"]
 
-function executeCommand(command, callBack) {
-    if (validCommands.indexOf(command) != -1) {
-        callBack()
+function executeCommand(command, successCallBack, failCallBack) {
+    var commandIndex = validCommands.indexOf(command);
+    if (commandIndex != -1) {
+        failCallBack()
+    } else if (commandIndex == 0) {
+        executeGo()
+    } else if (commandIndex == 1) {
+        executeTake()
+    } else if (commandIndex == 2) {
+        executeUser()
+    } else if (commandIndex == 3) {
+        executeInventory()
     }
 }
 
